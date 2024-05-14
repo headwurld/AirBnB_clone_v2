a41c193812112934c641d45146f58b65b00513c0
headwurld <ihedpros@gmail.com>
2024-05-14 16:37:56 +0100

    Airbnb_clone_v2

---
+++ _tests/test_models/test_amenity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" """
+from _tests.test_models.test_base_model import test_basemodel
+from _models.amenity import Amenity
+
+
+class test_Amenity(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Amenity"
+        self.value = Amenity
+
+    def test_name2(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
